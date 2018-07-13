cbc989bb66eeffecee473451e45e00d6ec6c0620
Daniel Schmidt <dschmidt@mesosphere.com>
2018-07-13 16:43:10 +0200

    chore(reactjs-components): add typings for Dropdown
    These typings are derived from the PropTypes

--- src/typings/reactjs-components.d.ts
+++ src/typings/reactjs-components.d.ts
@@ -13,8 +13,37 @@ declare module "reactjs-components" {
     useGemini: boolean;
   }
 
-  export class Modal extends Component<ModalProps, {}> {}
+  interface MenuItem {
+    className?: string;
+    html?: JSX.Element;
+    id: string | number;
+    onClick?: () => void;
+    selectable?: boolean;
+    selectedHtml?: string | object;
+  }
 
+  interface DropdownProps {
+    anchorRight?: boolean;
+    persistentID?: string | number;
+    items: MenuItem[];
+    initialID?: string | number;
+    matchButtonWidth?: boolean;
+    onItemSelection?: (...args: any[]) => any;
+    scrollContainer?: object | string;
+    scrollContainerParentSelector?: string;
+    transition?: boolean;
+    transitionName?: string;
+    transitionEnterTimeout?: number;
+    transitionLeaveTimeout?: number;
+    trigger?: JSX.Element;
+    useGemini?: boolean;
+    disabled?: boolean;
+    buttonClassName?: string;
+    dropdownMenuClassName?: string;
+    dropdownMenuListClassName?: string;
+    dropdownMenuListItemClassName?: string;
+    wrapperClassName?: string;
+  }
   interface TooltipProps {
     anchor?: string;
     children: React.ReactNode;
@@ -32,5 +61,8 @@ declare module "reactjs-components" {
     contentClassName?: string;
   }
 
+  export class Modal extends Component<ModalProps, {}> {}
+
   export class Tooltip extends Component<TooltipProps, {}> {}
+  export class Dropdown extends Component<DropdownProps, {}> {}
 }
